6f5e693a91bba97fdc32235e8f9b5fc97f1bdf2d
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2021-10-05 03:05:28 +0000

    Trying to get xml2 to work

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,3 +1,5 @@
 FROM gitpod/workspace-full
 
 RUN brew install R
+RUN sudo apt-get install libxml2-dev
+RUN R CMD BATCH test.r && cat test.r.Rout
--- test.r
+++ test.r
@@ -1,1 +1,7 @@
+install.packages("xml2", repos = "http://cran.us.r-project.org")
+install.packages("rvest", repos = "http://cran.us.r-project.org")
+
+
 print("hello ") 
+
+
