0dd97e95abf6c6e5dab34d642f73757d2e2263c5
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2021-10-05 03:16:14 +0000

    changing yml syntax to run in correct order.  No race conditioning please!

--- test.r
+++ test.r
@@ -1,6 +1,13 @@
 install.packages("xml2", repos = "http://cran.us.r-project.org")
 install.packages("rvest", repos = "http://cran.us.r-project.org")
 
+library(xml2)
+library(rvest)
+webpage_url <- "http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html"
+webpage <- xml2::read_html(webpage_url)
+ExOffndrsRaw <- rvest::html_table(webpage)[[1]] %>% 
+  tibble::as_tibble(.name_repair = "unique") # repair the repeated columns
+ExOffndrsRaw %>% dplyr::glimpse(45)
 
 print("hello ") 
 
